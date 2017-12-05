042d86b438b0c1d7125ddabdeffd3fc68b314f02
shbatgh <shboccara@gmail.com>
2017-12-04 20:18:34 -0500

    Create test.py

---
+++ test.py
@@ -0,0 +1,9 @@
+class Papa:
+    def __init__(self, stupidness, annoying):
+        self.stupidness = stupidness
+        self.annoying = annoying
+       
+    def papaDoes(self):
+        if self.stupidness == 10:
+            print("Papa is the biggest butt!!!")
+       
